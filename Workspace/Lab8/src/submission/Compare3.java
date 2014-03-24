b449fd0182603a9532332197fedd62f26f57f989
Leon <ho.leon@yahoo.com>
2014-03-24 08:20:32 -0700

    Nothing changed

--- Workspace/Lab8/src/submission/Compare3.java
+++ Workspace/Lab8/src/submission/Compare3.java
@@ -1,16 +1,9 @@
 package submission;
 
-public class Compare3 {
+public class Compare3 implements Comparable<String>{
     public static String largest(String x, String y , String z) {
-        String largest = x;
-        if(x.compareTo(y) > 0 && x.compareTo(z) > 0) {
-            largest = x;
-        } else if (y.compareTo(x) > 0 && y.compareTo(z) >0) {
-            largest = y;
-        } else {
-            largest = z;
+        static final String compareTo(String input) {
+            
         }
-        //System.out.println(x.compareTo(y));
-        return largest;
     }
 }
